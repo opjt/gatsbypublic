ed4208a93ffd885ee88da72aa480bbefd8cb26cf
opjt <jtpark1957@gmail.com>
2021-05-18 19:04:24 +0900

    hash mode

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 
 
@@ -20,7 +20,7 @@ const routes = [
 
 const router = createRouter({
   // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes, // short for `routes: routes`
 })
 
